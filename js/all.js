9d2ebf912f23079905ad85bf62d9bcabe472a102
gp355d <chichali374@gmail.com>
2017-01-19 10:35:17 +0800

    練習2
    demo

---
+++ js/all.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+	$('.open').click(function(event) {
+    /* Act on the event */
+    event.preventDefault();
+    $('.open').toggleClass('active');
+    $('.op').slideToggle();
+  });
+});
+$(document).ready(function() {
+	$('.open1').click(function(event) {
+    /* Act on the event */
+    event.preventDefault();
+    $('.open1').toggleClass('active');
+    $('.op1').slideToggle();
+  });
+});
+$(document).ready(function() {
+	$('.open2').click(function(event) {
+    /* Act on the event */
+    event.preventDefault();
+    $('.open2').toggleClass('active');
+    $('.op2').slideToggle();
+  });
+});
+$(document).ready(function() {
+	$('.open3').click(function(event) {
+    /* Act on the event */
+    event.preventDefault();
+    $('.open3').toggleClass('active');
+    $('.op3').slideToggle();
+  });
+});
+$(document).ready(function() {
+	$('.open4').click(function(event) {
+    /* Act on the event */
+    event.preventDefault();
+    $('.open4').toggleClass('active');
+    $('.op4').slideToggle();
+  });
+});
+$(document).ready(function() {
+	$('.open5').click(function(event) {
+    /* Act on the event */
+    event.preventDefault();
+    $('.open5').toggleClass('active');
+    $('.op5').slideToggle();
+  });
+});5
